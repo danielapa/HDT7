154dc4b9f5dfcd16ea3f8b228c8c003b524617ed
dbs_jd <dbs_jd@172.20.14.240>
2015-09-09 16:00:41 -0600

    JUnits terminados

--- Hoja7/test/hoja7/Hoja7Test.java
+++ Hoja7/test/hoja7/Hoja7Test.java
@@ -48,12 +48,14 @@ public class Hoja7Test {
      */
     @Test
     public void testInsert() {
-        Map arreglo[] = new TreeMap[2]; //se crea un map con cada una de las palabras
+        Map arreglo[] = new TreeMap[3]; //se crea un map con cada una de las palabras
         
-        String palabrain = "cat";
-        String palabraesp = "gato";
+        String palabrain = "star";
+        String palabraesp = "estrella";
         String palabrainL = "dog";
         String palabraespL = "perro";
+        String palabrainLL = "pillow";
+        String palabraespLL = "almohada";
         
         arreglo[0] = new TreeMap();
         arreglo[0].put(palabraesp,palabrain);
@@ -61,17 +63,26 @@ public class Hoja7Test {
         arreglo[1] = new TreeMap();
         arreglo[1].put(palabraespL,palabrainL);
         
+        arreglo[2] = new TreeMap();
+        arreglo[2].put(palabraespLL, palabrainLL);
+        
         //se crea la raiz del arbol
         System.out.println("--");
         BinaryTree<Map>[] hojastree; //se crean hojas con el numero de palabras restantes
-        hojastree = new BinaryTree[1];
+        hojastree = new BinaryTree[2];
         BinaryTree<Map> root = new BinaryTree<>(arreglo[0]);
         
         hojastree[0] = new BinaryTree(arreglo[1]);
-        
+        hojastree[1] = new BinaryTree(arreglo[2]);
         boolean expResult = true;
-        boolean result = root.insertar(root,hojastree[0]);
         
+        boolean result = root.insertar(root,hojastree[0]);
+        root.insertar(root,hojastree[1]);
+        if (expResult == true){
+            System.out.println("Prueba con exito. expResult: " + expResult + " result: " + result);
+            System.out.println("Objetos en el arbol: ");
+            root.inOrder(root);
+        }
         assertEquals(expResult, result);        
     }
     
@@ -113,8 +124,8 @@ public class Hoja7Test {
         String expResult = null; //Que no se encuentra en el arbol
         
         String result = root.buscar(root, palabra);
-        System.out.println("expResult: " + expResult);
-        System.out.println("Result: " + result);
+       // System.out.println("expResult: " + expResult);
+        //System.out.println("Result: " + result);
 
 
         assertEquals(expResult, result);        
